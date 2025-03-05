0d40fe1912cae77616af23c2f8cf47498ee07a95
Rick <leopcrick@gmail.com>
2025-03-05 19:26:02 -0300

    avanço desafios

---
+++ Desafios próprios/desafiando devs/d09-SepararTempoEmSeg.js
@@ -0,0 +1,43 @@
+//POSSIBILITA A ENTRADA DE DADOS NO TERMINAL
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//FUNÇÃO VERIFICAR
+function verificar(){
+    if (1==1){
+        return true
+    }
+}
+
+//DIVIDIR O TEMPO
+function tempo(input){
+    let suporte = input.split(":");
+    const hora = Number(suporte[0]);
+    const minutos = Number(suporte[1]);
+    const segundos = Number(suporte[2]);
+    const tempoTotal = hora*3600 + minutos*60 + segundos
+    console.log("O tempo total do input é de " + tempoTotal + " segundos");
+}
+
+//CÓDIGO GERAL
+function rodar(){
+    console.log("Digite um tempo no formato HH:MM:SS")
+    rl.question('Digite seu número: ', (input) => {
+        if (verificar(input)){
+            tempo(input)
+            console.log("Parabéns! Deu certo.")
+            rl.close();
+        } else {
+            process.stdout.write("O número fornecido não está entre 1 e 20.")
+            console.log("")
+            rodar()
+        }
+    });
+}
+
+rodar()
+
+//TEMPO: 15minutos sem verificação
